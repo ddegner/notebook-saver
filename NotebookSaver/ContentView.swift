527e024d7aa26aea0b4243052aa9773e883883b0
David Degner <david@daviddegner.com>
2025-05-01 21:42:56 -0400

    Initial Commit

---
+++ NotebookSaver/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NotebookSaver
+//
+//  Created by David Degner on 5/1/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NotebookSaver/NotebookSaverApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotebookSaverApp.swift
+//  NotebookSaver
+//
+//  Created by David Degner on 5/1/25.
+//
+
+import SwiftUI
+
+@main
+struct NotebookSaverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
